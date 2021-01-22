904d02a62cc3542b6f4d28c0b3e382b028c4411c
Daniel Justiz <danieljustiz9508@gmail.com>
2021-01-22 07:33:14 -0500

    Edit

--- app.js
+++ app.js
@@ -21,3 +21,20 @@ if (!address) {
         })
     })
 }
+
+
+// Object destructuring
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined
+}
+
+// const {label:productLabel, stock, rating = 5} = product
+// console.log(label, stock)
+
+const transaction = (type, { label, stock }) => {
+    console.log(type, label, stock)
+}

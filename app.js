2589486c2c56f97c810712f353adfc02b6d44e16
danielJ305 <danieljustiz9508@gmail.com>
2021-01-22 16:55:41 -0500

    Progress

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const address = process.argv[2];
 if (!address) {
     console.log('Please provide an address')
 } else {
-    geocode(address, (error, data) => {
+    geocode(address, (error, {latitude, longitude, location}) => {
         if (error) {
             return console.log(error)
         }
 
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return console.log(error);
             }
 
-            console.log(data.location)
+            console.log(location)
             console.log(forecastData)
         })
     })
@@ -38,3 +38,5 @@ const product = {
 const transaction = (type, { label, stock }) => {
     console.log(type, label, stock)
 }
+
+transaction('order', product);
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=56afca3b3603626dcd4df4212846ca49&query=${latitude},${longitude}&units=f`;
 
-    request({ url: url, json: true }, (error, response ) => {
+    request({ url, json: true }, (error, {body} ) => {
         if (error) {
             callback('Unable to connect to forecast services!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find forecast. Try another search.', undefined)
         } else {
-            callback(undefined, `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degress out. There is a ${response.body.current.precip} chance of rain`);
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. There is a ${body.current.precip} chance of rain`);
         }
     })
 }

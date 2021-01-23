40500fd5e0da21b08df7dfcef799c30bb1f00cc1
Daniel Justiz <danieljustiz9508@gmail.com>
2021-01-23 11:32:42 -0500

    working on web servers folder

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,31 +1,10 @@
 const path = require('path')
 const express = require('express')
-
 const app = express();
+
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath))
-
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
-
-app.get('/help', (req, res) => {
-    res.send({
-        name: 'Daniel',
-        age: 26
-    })
-})
-
-app.get('/about', (req, res) => {
-    res.send([{
-            name: 'Daniel'
-        },
-        {
-            name: 'Poseidon'
-        }
-    ])
-})
 
 app.get('/weather', (req, res) => {
     res.send('Weather page')

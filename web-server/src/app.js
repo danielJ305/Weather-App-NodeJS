31d6fa96ac68767fd36d728cf9cbc5712bc46afe
Daniel Justiz <danieljustiz9508@gmail.com>
2021-01-24 18:41:42 -0500

    JSON HTTP Endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,100 @@
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
+
 const path = require('path')
 const express = require('express')
-const app = express();
+const hbs = require('hbs')
 
-const publicDirectoryPath = path.join(__dirname, '../public');
+const app = express()
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '/templates/views')
+const partialsPath = path.join(__dirname, '/templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('/weather', (req, res) => {
-    res.send('Weather page')
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Daniel Justiz'
+    })
 })
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Daniel Justiz'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Daniel Justiz'
+    })
+})
+
+// app.get('/products', (req, res) => {
+//     if (!req.query.search) {
+//         return res.send({
+//             error: 'You must provide a search term'
+//         })
+//     }
+//     console.log(req.query)
+//     res.send({
+//         products: []
+//     })
+// })
+
+app.get('/weather', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
+    geocode(req.query.search, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({
+                error: error
+            })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({
+                    error: error
+                })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location: location
+            })
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404page', {
+        message: 'Help article not found!'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404page', {
+        message: 'My 404 page'
+    })
+})
+
 
 // app.com
 // app.com/help
